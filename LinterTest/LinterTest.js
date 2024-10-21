cf4b699ece32ca747a3b4f3a872e146edf4fdca3
Flo <enirolf254@gmail.com>
2024-10-21 11:19:38 +0200

    HavefunwithCode2!

--- LinterTest/LinterTest.js
+++ LinterTest/LinterTest.js
@@ -1,16 +1,17 @@
+"use strict";
 //Linter_Test
 var LinterTest;
 (function (LinterTest) {
-    var KEY;
+    let KEY;
     (function (KEY) {
         KEY[KEY["POS"] = 1] = "POS";
         KEY[KEY["NEG"] = -1] = "NEG";
     })(KEY || (KEY = {}));
-    var info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
+    const info = { text: "G`udetmvhsgBncd1 ", key: KEY.POS };
     console.log(deCrypt(info.text, info.key));
     function deCrypt(_text, _key) {
-        var result = "";
-        for (var i = 0; i < _text.length; i++) {
+        let result = "";
+        for (let i = 0; i < _text.length; i++) {
             result += String.fromCharCode(_text.charCodeAt(i) + _key);
         }
         return result;
