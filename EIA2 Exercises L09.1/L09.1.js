4777d9d87c3123a4361a1e43bfa5db1abc1cac95
Flo <enirolf254@gmail.com>
2024-10-23 22:27:17 +0200

    SerahSendHerRegards

--- EIA2 Exercises L09.1/L09.1.js
+++ EIA2 Exercises L09.1/L09.1.js
@@ -1,6 +1,30 @@
-var Vektoren;
-(function (Vektoren) {
-    var v1 = new Vector();
-    v1.scale(2);
-    console.log(v1);
-})(Vektoren || (Vektoren = {}));
+var DnDKlassen;
+(function (DnDKlassen) {
+    var Krieger = /** @class */ (function () {
+        function Krieger() {
+        }
+        //Methoden der Klasse
+        //Bauplan von Objektaktionen 
+        Krieger.prototype.levelUp = function (_factor) {
+            this.x *= _factor; //this bezieht sich auf die Werte innerhalb der Klasse, du musst es schreiben da das was du in der Methode angibst zwar ähnlich heißen kann, aber icht dasselbe ist. Ergo musst du dir veränderung dem ursprungswert zuweisen. Grüé an Jirka
+            this.y *= _factor;
+        };
+        ;
+        Krieger.prototype.nope = function (_addend) {
+            this.x += _addend.x;
+            this.y += _addend.y;
+        };
+        ;
+        Krieger.prototype.firstLevel = function (_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        };
+        return Krieger;
+    }());
+    var aragorn = new Krieger(); //Erstellen eines neuen Objektes (Vektor) basierend auf dem Bauplan
+    aragorn.firstLevel(5, 5);
+    console.log("Ich bin ein krieger oder so");
+    console.log(aragorn);
+    aragorn.levelUp(2);
+    console.log(aragorn);
+})(DnDKlassen || (DnDKlassen = {}));
--- EIA2 Exercises L09.1/L09.1.ts
+++ EIA2 Exercises L09.1/L09.1.ts
@@ -1,7 +1,36 @@
-namespace Vektoren {
+namespace DnDKlassen {
 
-    let v1: Vector = new Vector();
-    v1.scale(2);
-    console.log(v1);
+    class Krieger { //allgemeine Baupläne für alle Objekte dieser Klasse
+        //Attribute der Klasse
+        //Bauplan Objekteigenschaften
+        x: number ; 
+        y: number ;
+    
+        //Methoden der Klasse
+        //Bauplan von Objektaktionen 
+       levelUp (_factor: number): void { 
+            this.x *= _factor; //this bezieht sich auf die Werte innerhalb der Klasse, du musst es schreiben da das was du in der Methode angibst zwar ähnlich heißen kann, aber icht dasselbe ist. Ergo musst du dir veränderung dem ursprungswert zuweisen. Grüé an Jirka
+            this.y *= _factor;
+        };
+    
+        nope (_addend: Krieger): void {
+            this.x += _addend.x;
+            this.y += _addend.y;
+        };
+
+        firstLevel (_x: number, _y: number): void {
+           this.x = _x;
+           this.y = _y;
+        }
+    }
+
+    const aragorn: Krieger = new Krieger(); //Erstellen eines neuen Objektes (Vektor) basierend auf dem Bauplan
+    aragorn.firstLevel(5,5)
+    console.log("Ich bin ein krieger oder so")
+    console.log(aragorn)
+    aragorn.levelUp(2);
+    console.log(aragorn);
+
+    
 
 }
