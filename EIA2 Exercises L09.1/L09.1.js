c93ee0a93b3efe700ae70f547f7aed86beeb8905
Flo <enirolf254@gmail.com>
2024-10-23 21:37:04 +0200

    L09.1

---
+++ EIA2 Exercises L09.1/L09.1.js
@@ -0,0 +1,6 @@
+var Vektoren;
+(function (Vektoren) {
+    var v1 = new Vector();
+    v1.scale(2);
+    console.log(v1);
+})(Vektoren || (Vektoren = {}));
---
+++ EIA2 Exercises L09.1/L09.1.ts
@@ -0,0 +1,7 @@
+namespace Vektoren {
+
+    let v1: Vector = new Vector();
+    v1.scale(2);
+    console.log(v1);
+
+}
