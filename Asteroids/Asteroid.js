b37ed7f9e039915ff0db92ca6cd0ff20129c71a6
Flo <enirolf254@gmail.com>
2024-10-28 12:56:45 +0100

    corrected the links in the html file (to the asteroid class)

--- Asteroids/Asteroid.js
+++ Asteroids/Asteroid.js
@@ -7,6 +7,8 @@ var Asteroid_Project;
             this.position = new Asteroid_Project.Vector(0, 0);
             this.velocity = new Asteroid_Project.Vector(0, 0);
             this.velocity.random(100, 200); //Asteroiden sollen 100 bis 200 Pixel pro Sekunde fliegen können
+            this.type = Math.floor(Math.random() * 4); //wir haben vier Pfade, von denen der Asteroid random einen aussuchen soll
+            this.size = _size;
         }
         move(_timeslice) {
             console.log("Asteroid movement");
--- Asteroids/Asteroid.ts
+++ Asteroids/Asteroid.ts
@@ -9,10 +9,12 @@ namespace Asteroid_Project {
 
         constructor(_size: number) {
             console.log("asteroid constructor"); //console.log to check if the method works
-            this.position = new Vector (0,0);
-            this.velocity = new Vector (0,0);
+            this.position = new Vector(0, 0);
+            this.velocity = new Vector(0, 0);
             this.velocity.random(100, 200); //Asteroiden sollen 100 bis 200 Pixel pro Sekunde fliegen können
-            
+
+            this.type = Math.floor(Math.random() * 4); //wir haben vier Pfade, von denen der Asteroid random einen aussuchen soll
+            this.size = _size;
         }
 
         move(_timeslice: number): void {
