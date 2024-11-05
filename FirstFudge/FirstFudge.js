285a942470438fd190706c3ae356c22505463dc5
Flo <enirolf254@gmail.com>
2024-11-05 15:49:29 +0100

    fudge

--- FirstFudge/FirstFudge.js
+++ FirstFudge/FirstFudge.js
@@ -4,15 +4,33 @@ var FirstFudge;
     var f = FudgeCore;
     console.log(f);
     window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
+    f.Loop.addEventListener("loopFrame" /* f.EVENT.LOOP_FRAME */, moveCube);
     function start() {
         const canvas = document.querySelector("canvas");
         console.log(canvas);
-        const cube = new f.MeshCube("Cube");
-        console.log(cube);
+        const mesh = new f.MeshCube("Mesh");
+        console.log(mesh);
         const camera = new f.ComponentCamera();
         console.log(camera);
+        const node = new f.Node("Node");
+        const compMesh = new f.ComponentMesh(mesh);
+        node.addComponent(compMesh);
+        const material = new f.Material("Material", f.ShaderLit);
+        const compMaterial = new f.ComponentMaterial(material);
+        compMaterial.clrPrimary.set(0.8, 0.7, 0.9, 0.5);
+        node.addComponent(compMaterial);
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180);
+        node.addComponent(new f.ComponentTransform());
+        node.getComponent(f.ComponentTransform).mtxLocal.translateX(2);
+        //the shortcut to line above: node.mtxLocal.translateX(2);
+        //another version is to save it in a variable first const compTransform: f.ComponentTransform = new f.ComponentTransform(); node.addComponent(compTransform);
+        console.log(node);
         const viewport = new f.Viewport();
-        viewport.initialize("Viewport", null, camera, canvas);
+        viewport.initialize("Viewport", node, camera, canvas);
+        viewport.draw();
+    }
+    function moveCube() {
     }
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=FirstFudge.js.map
--- FirstFudge/FirstFudge.ts
+++ FirstFudge/FirstFudge.ts
@@ -2,19 +2,47 @@ namespace FirstFudge {
     import f = FudgeCore;
     console.log(f);
 
+
     window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
+
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, moveCube);
 
     function start(): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
         console.log(canvas);
 
-        const cube: f.MeshCube = new f.MeshCube("Cube");
-        console.log(cube);
+        const mesh: f.MeshCube = new f.MeshCube("Mesh");
+        console.log(mesh);
 
         const camera: f.ComponentCamera = new f.ComponentCamera();
         console.log(camera);
 
+        const node: f.Node = new f.Node("Node");
+
+        const compMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(compMesh);
+
+        const material: f.Material = new f.Material("Material", f.ShaderLit);
+        const compMaterial: f.ComponentMaterial = new f.ComponentMaterial(material);
+        compMaterial.clrPrimary.set(0.8, 0.7, 0.9, 0.5);
+        node.addComponent(compMaterial);
+
+        camera.mtxPivot.translateZ(5);
+        camera.mtxPivot.rotateY(180);
+
+        node.addComponent(new f.ComponentTransform());
+        node.getComponent(f.ComponentTransform).mtxLocal.translateX(2);
+        //the shortcut to line above: node.mtxLocal.translateX(2);
+        //another version is to save it in a variable first const compTransform: f.ComponentTransform = new f.ComponentTransform(); node.addComponent(compTransform);
+
+        console.log(node);
+
         const viewport: f.Viewport = new f.Viewport();
-        viewport.initialize("Viewport", null, camera, canvas);
+        viewport.initialize("Viewport", node, camera, canvas);
+        viewport.draw();
+    }
+
+    function moveCube(): void {
+
     }
 }
