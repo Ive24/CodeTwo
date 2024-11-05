ef829cd44701755295a4cd96f3f6ac0a65e1584c
Flo <enirolf254@gmail.com>
2024-11-05 11:18:48 +0100

    fudge

--- FirstFudge/FirstFudge.js
+++ FirstFudge/FirstFudge.js
@@ -3,5 +3,16 @@ var FirstFudge;
 (function (FirstFudge) {
     var f = FudgeCore;
     console.log(f);
+    window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
+    function start() {
+        const canvas = document.querySelector("canvas");
+        console.log(canvas);
+        const cube = new f.MeshCube("Cube");
+        console.log(cube);
+        const camera = new f.ComponentCamera();
+        console.log(camera);
+        const viewport = new f.Viewport();
+        viewport.initialize("Viewport", null, camera, canvas);
+    }
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=FirstFudge.js.map
--- FirstFudge/FirstFudge.ts
+++ FirstFudge/FirstFudge.ts
@@ -1,4 +1,20 @@
 namespace FirstFudge {
     import f = FudgeCore;
     console.log(f);
+
+    window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
+
+    function start(): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+        console.log(canvas);
+
+        const cube: f.MeshCube = new f.MeshCube("Cube");
+        console.log(cube);
+
+        const camera: f.ComponentCamera = new f.ComponentCamera();
+        console.log(camera);
+
+        const viewport: f.Viewport = new f.Viewport();
+        viewport.initialize("Viewport", null, camera, canvas);
+    }
 }
