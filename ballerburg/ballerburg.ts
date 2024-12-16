99e55ef22c8cc92d9e5914a949cb8b0c76c5f6ea
Flo <enirolf254@gmail.com>
2024-12-16 16:52:13 +0100

    fixed

--- ballerburg/ballerburg.ts
+++ ballerburg/ballerburg.ts
@@ -8,9 +8,9 @@ const posPlayers: number[] = [];
 const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0]!;
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-const peak: number = Math.floor(Math.random()*canvas.height*0.5)+canvas.height*0.25;
-const platform1: number = Math.floor(Math.random()*canvas.height*0.5)+canvas.height*0.5;
-const platform2: number = Math.floor(Math.random()*canvas.height*0.5)+canvas.height*0.5;
+const peak: number = Math.floor(Math.random() * canvas.height * 0.5) + canvas.height * 0.25;
+const platform1: number = Math.floor(Math.random() * canvas.height * 0.5) + canvas.height * 0.5;
+const platform2: number = Math.floor(Math.random() * canvas.height * 0.5) + canvas.height * 0.5;
 
 // Slider Variables
 let slider1: HTMLInputElement = <HTMLInputElement>document.getElementById("angle1");
@@ -51,20 +51,20 @@ interface Ball {
 }
 
 // Slider Values on Input
-slider1.oninput = function(): void {
-  angle1 = Number(slider1.value);
-  console.log(angle1); 
+slider1.oninput = function (): void {
+    angle1 = Number(slider1.value);
+    console.log(angle1);
 }
 
-slider2.oninput = function(): void {
+slider2.oninput = function (): void {
     angle2 = Number(slider2.value);
 }
 
-slider3.oninput = function(): void {
+slider3.oninput = function (): void {
     gunpower1 = Number(slider3.value);
 }
 
-slider4.oninput = function(): void {
+slider4.oninput = function (): void {
     gunpower2 = Number(slider4.value);
 }
 
@@ -77,16 +77,16 @@ function handleLoad(): void {
 
 function generateCannons(_amount: number): void {
 
-    const pos1: number = canvas.width*0.15;
-    const pos2: number = canvas.width*0.85;
+    const pos1: number = canvas.width * 0.15;
+    const pos2: number = canvas.width * 0.85;
 
     posPlayers.push(pos1);
     posPlayers.push(pos2);
 
-    for (let i: number = 0; i<_amount; i++) {
+    for (let i: number = 0; i < _amount; i++) {
         const newCannon: Cannon = {
 
-            player: i+1,
+            player: i + 1,
             posX: posPlayers[i],
             posY: 0,
             path: new Path2D,
@@ -108,7 +108,7 @@ function generateBall(_cannon: Cannon): void {
         radius: 5,
         posX: _cannon.posX,
         posY: _cannon.posY,
-        shoot:false
+        shoot: false
 
     }
     cannonBalls.push(newBall);
@@ -118,11 +118,11 @@ function generateBall(_cannon: Cannon): void {
 function drawTerrain(): void {
 
     ctx.beginPath();
-    ctx.moveTo(0,platform1);
-    ctx.lineTo(canvas.width*0.3,platform1);
-    ctx.lineTo(Math.floor(Math.random()*canvas.width*0.3)+canvas.width*0.3, peak);
-    ctx.lineTo(canvas.width*0.7,platform2);
-    ctx.lineTo(canvas.width,platform2);
+    ctx.moveTo(0, platform1);
+    ctx.lineTo(canvas.width * 0.3, platform1);
+    ctx.lineTo(Math.floor(Math.random() * canvas.width * 0.3) + canvas.width * 0.3, peak);
+    ctx.lineTo(canvas.width * 0.7, platform2);
+    ctx.lineTo(canvas.width, platform2);
 
     ctx.strokeStyle = "black";
     ctx.stroke();
@@ -133,7 +133,7 @@ function drawTerrain(): void {
 //draws cannons
 function drawCannons(): void {
 
-    for (let i: number = 0; i<cannons.length; i++) {
+    for (let i: number = 0; i < cannons.length; i++) {
 
         const cannon: Cannon = cannons[i];
         const x: number = cannon.posX;
@@ -141,7 +141,7 @@ function drawCannons(): void {
         const angle: number = 0;
         const cannonRadius: number = 15;
 
-        cannon.path.moveTo(x,y);
+        cannon.path.moveTo(x, y);
         cannon.path.arc(x, y, cannonRadius, 0, 360);
 
 
@@ -158,7 +158,7 @@ function drawBalls(): void {
 
 //fires the cannon
 function fireCannon(_cannon: Cannon): void {
-    
+
 }
 
 //calculates the balls flight path if cannon is fired
@@ -167,12 +167,12 @@ function ballFly(_ball: Ball): void {
     if (_ball.shoot)
 
 
-           
-            _ball.posX += _ball.speed;
-            _ball.posY += gravity;
 
-            collisionCheck(_ball);
-            }
+        _ball.posX += _ball.speed;
+    _ball.posY += gravity;
+
+    collisionCheck(_ball);
+}
     
         }
     }
@@ -193,7 +193,7 @@ function winner(_player: number): void {
 function animationFrame(): void {
     drawCannons();
     drawBalls();
-    for (let i:number = 0; i<cannonBalls.length; i++) {
+    for (let i: number = 0; i < cannonBalls.length; i++) {
         ballFly(cannonBalls[i]);
     }
 
